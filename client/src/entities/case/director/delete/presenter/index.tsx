1afa4f0f0fb035cf82603d5c50c6bdbc5a72aeaf
PrSalvator <kirill.leono2016yandex.ru@gmail.com>
2024-08-22 12:38:02 +0600

    fix delete director message

--- client/src/entities/case/director/delete/presenter/index.tsx
+++ client/src/entities/case/director/delete/presenter/index.tsx
@@ -30,7 +30,7 @@ export const useDeleteDirectorPresenter = () => {
     return (
       director && (
         <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Удаление директора</DialogTitle>
+          <DialogTitle>Удаление начальника</DialogTitle>
           <DialogContent>
             Вы уверены, что хотите удалить начальника цеха "{getFio(director!)}"
             ?
